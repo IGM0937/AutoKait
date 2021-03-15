fd810516edd93f398617ec7580050738157f374e
Igor Goran Macukat <igor@mgor.co.uk>
2021-03-15 10:31:59 +0000

    update to the constants class, moving variables around. no functional change

--- start.py
+++ start.py
@@ -1,42 +1,42 @@
 from random import randint
 
-import util.constants as const
 import util.global_vars as global_vars
 import util.output_text as output
+from util.constants import *
 
 
 def explain_action():
     action = global_vars.last_action
-    if action == const.ACTION_SPECIAL_INTEREST:
+    if action == ACTION_SPECIAL_INTEREST:
         print(output.special_interest_action_explain_text())
-    elif action == const.ACTION_PLACE_TRACKS:
+    elif action == ACTION_PLACE_TRACKS:
         print(output.place_tracks_action_explain_text())
-    elif action == const.ACTION_CALL_AUCTION:
+    elif action == ACTION_CALL_AUCTION:
         print(output.call_auction_action_explain_text())
-    elif action == const.ACTION_CALL_DIVIDENDS:
+    elif action == ACTION_CALL_DIVIDENDS:
         print(output.call_dividends_action_explain_text())
 
 
 def special_interest_action():
     print(output.special_interest_action_text())
-    global_vars.last_action = const.ACTION_SPECIAL_INTEREST
+    global_vars.last_action = ACTION_SPECIAL_INTEREST
 
 
 def place_tracks_action():
     print(output.place_tracks_action_text())
-    global_vars.last_action = const.ACTION_PLACE_TRACKS
+    global_vars.last_action = ACTION_PLACE_TRACKS
 
 
 def call_auction_action():
     print(output.call_auction_action_text())
-    global_vars.last_action = const.ACTION_CALL_AUCTION
+    global_vars.last_action = ACTION_CALL_AUCTION
     if not ask_user_prompt(output.call_auction_action_be_performed_text()):
         call_dividends_action()
 
 
 def call_dividends_action():
     print(output.call_dividends_action_text())
-    global_vars.last_action = const.ACTION_CALL_DIVIDENDS
+    global_vars.last_action = ACTION_CALL_DIVIDENDS
     if not ask_user_prompt(output.call_dividends_action_be_performed_text()):
         special_interest_action()
 
@@ -44,24 +44,18 @@ def call_dividends_action():
 def ask_user_prompt(text):
     while True:
         answer = input(text).lower()
-        if answer == const.YES_SRT or answer == const.YES_LNG:
+        if answer == YES_SRT or answer == YES_LNG:
             return True
-        elif answer == const.NO_SRT or answer == const.NO_LNG:
+        elif answer == NO_SRT or answer == NO_LNG:
             return False
-        elif answer == const.EXIT:
+        elif answer == EXIT:
             print(output.exit_text(True))
             exit()
-        elif answer == const.EXPLAIN:
+        elif answer == EXPLAIN:
             explain_action()
         else:
             print(output.invalid_input())
             continue
-
-
-special_interest_act_range = range(1, 2)
-place_tracks_act_range = range(2, 5)
-call_auction_act_range = range(5, 6)
-call_dividends_act_range = range(6, 7)
 
 
 def roll_dice(dice_max):
@@ -75,13 +69,13 @@ def take_turn():
     result = roll_dice(6)
     print(output.cait_turn_text(), end='')
 
-    if result in special_interest_act_range:
+    if result in DICE_RANGE_SPECIAL_INTEREST:
         special_interest_action()
-    elif result in place_tracks_act_range:
+    elif result in DICE_RANGE_PLACE_TRACKS:
         place_tracks_action()
-    elif result in call_auction_act_range:
+    elif result in DICE_RANGE_CALL_AUCTION:
         call_auction_action()
-    elif result in call_dividends_act_range:
+    elif result in DICE_RANGE_CALL_DIVIDENDS:
         call_dividends_action()
 
 
@@ -92,10 +86,10 @@ def perform_setup():
 def start_event_loop():
     while True:
         current_input = input(output.cait_waiting_turn_text()).lower()
-        if current_input == const.EXIT:
+        if current_input == EXIT:
             print(output.exit_text(False))
             exit()
-        if current_input == const.EXPLAIN:
+        if current_input == EXPLAIN:
             explain_action()
         else:
             take_turn()
--- util/constants.py
+++ util/constants.py
@@ -1,4 +1,8 @@
-# inputs
+"""
+List of constant variables
+"""
+
+# user inputs
 YES_SRT = 'y'
 YES_LNG = 'yes'
 NO_SRT = 'n'
@@ -6,8 +10,14 @@ NO_LNG = 'no'
 EXIT = 'exit'
 EXPLAIN = 'explain'
 
-# actions
+# actions tags
 ACTION_SPECIAL_INTEREST = 'action.special_interest'
 ACTION_PLACE_TRACKS = 'action.place_tracks'
 ACTION_CALL_AUCTION = 'action.place_auction'
 ACTION_CALL_DIVIDENDS = 'action.call_dividends'
+
+# dice actions range
+DICE_RANGE_SPECIAL_INTEREST = range(1, 2)
+DICE_RANGE_PLACE_TRACKS = range(2, 5)
+DICE_RANGE_CALL_AUCTION = range(5, 6)
+DICE_RANGE_CALL_DIVIDENDS = range(6, 7)
