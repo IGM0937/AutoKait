f3b5a6f79639f7351b3d809bf3bfe1e5d6aa4280
Igor Goran Macukat <igor@mgor.co.uk>
2021-06-24 16:40:23 +0100

    added licences prior to GitHub upload

--- action/auction.py
+++ action/auction.py
@@ -1,4 +1,23 @@
 """
+Program Name:   AutoKait
+Description:    The automa A.I. player designed to be used for the board game, Irish Gauge.
+Author:         Igor Goran Mačukat
+Filename:       auction.py
+
+Copyright (C) 2021
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not,
+see <https://www.gnu.org/licenses/>.
+
+----
+
 Containing the fields, methods and algorithm pertaining to making a decision to which company to auction for.
 
 As is: It's purely based on the company with most connections, and decided by the human players.
