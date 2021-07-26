84d6b37470b283f8124480368d33200ac7173520
Igor Goran Macukat <igor@mgor.co.uk>
2021-07-26 12:27:06 +0100

    poc for clickable images with radius bboxes

---
+++ tkinter/poc.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+
+
+is_selected = False
+
+
+def on_click(value=None):
+    global is_selected
+
+    if value is None:
+        value = is_selected
+
+    if value:
+        # change image on canvas
+        canvas.itemconfig(image_id, image=unselected_img)
+    else:
+        canvas.itemconfig(image_id, image=selected_img)
+
+    is_selected = not value
+
+
+# Taken from https://blog.furas.pl/python-tkinter-update-image-on-canvas-with-button-click-gb.html
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    # create images
+    unselected_img = tk.PhotoImage(file="ball1.gif")
+    selected_img = tk.PhotoImage(file="ball2.gif")
+
+    # canvas for image
+    canvas = tk.Canvas(root, width=60, height=60)
+
+    # set first image on canvas
+    image_id = canvas.create_image(0, 0, anchor='nw', image=unselected_img)
+    # Events list https://www.python-course.eu/tkinter_events_binds.php
+    canvas.tag_bind(image_id, '<Button-1>', lambda e: on_click())
+
+    # pack canvas onto window
+    canvas.pack()
+
+    # button to change image
+    button1 = tk.Button(root, text="Force Green", command=lambda: on_click(False))
+    button1.pack()
+    button2 = tk.Button(root, text="Force Red", command=lambda: on_click(True))
+    button2.pack()
+
+    root.mainloop()
---
+++ tkinter/poc2.py
@@ -0,0 +1,65 @@
+import tkinter as tk
+import math
+
+
+WIDTH = 60
+HEIGHT = 60
+RADIUS_BOUND = WIDTH/2
+is_selected = False
+mouse_x = None
+mouse_y = None
+
+
+def mouse_update(event):
+    global mouse_x
+    global mouse_y
+
+    mouse_x = event.x
+    mouse_y = event.y
+
+
+def on_click(value=None):
+    if not inbound():
+        return
+
+    global is_selected
+
+    if value is None:
+        value = is_selected
+
+    canvas.itemconfig(image_id, image=(unselected_img if value else selected_img))
+    is_selected = not value
+
+
+def inbound():
+    global mouse_x
+    global mouse_y
+    pos = canvas.coords(image_id)
+    centre_x = pos[0]
+    centre_y = pos[1]
+
+    x = centre_x - mouse_x
+    y = centre_y - mouse_y
+    dis = math.sqrt((x ** 2) + (y ** 2))
+
+    return dis < RADIUS_BOUND
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+
+    # create images
+    unselected_img = tk.PhotoImage(file="ball1.gif")
+    selected_img = tk.PhotoImage(file="ball2.gif")
+
+    # canvas for image
+    canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+    # set first image on canvas
+    image_id = canvas.create_image(WIDTH/2, HEIGHT/2, image=unselected_img)
+    canvas.tag_bind(image_id, '<Button-1>', lambda e: on_click())
+    canvas.bind('<Enter>', mouse_update)
+    canvas.bind('<Motion>', mouse_update)
+    # pack canvas onto window
+    canvas.pack()
+
+    root.mainloop()
